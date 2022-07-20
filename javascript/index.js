5d63b0973ab7abb810a8a16b9f3293a4ac21e170
Utahjezz <jazz@kleomed.es>
2022-07-20 19:30:36 +0200

    remove unused

--- javascript/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.main = void 0;
-var cosmwasm_stargate_1 = require("@cosmjs/cosmwasm-stargate");
-function main() {
-    var client = (0, cosmwasm_stargate_1["default"])();
-}
-exports.main = main;
